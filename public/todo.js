ebebde855bd07c0d3d761b16c028516f73b99059
TAK <infernal-slam@hotmail.com>
2016-04-08 14:53:04 +0700

    fix

--- public/todo.js
+++ public/todo.js
@@ -1,47 +1,38 @@
 /* global angular alert moment*/
 angular.module('todoApp', [])
-  .controller('TodoListController', function ($http, $interval) {
-    var todoList = this
-    todoList.title = 'Hotel booking'
+  .controller('appController', function ($http, $interval) {
+    var app = this
+    app.title = 'Hotel booking'
     get()
     $interval(function () {
       get()
     }, 3000)
-    todoList.add = function (input) {
+    app.add = function (input) {
       save(input)
     }
     function save (data) {
-      $http.post('/api/data', data) // insert object
+      $http.post('/api/data', data)
         .then(function success (response) {
           console.log(response)
           get()
           alert('Success')
-        }, function error (response) {
-          alert(response.data.message)
         })
     }
     function get () {
       $http.get('/api/data')
         .then(function success (response) {
-          todoList.getData = response.data
-        }, function error (response) {
-          alert(response.data.message)
+          app.getData = response.data
         })
     }
-    todoList.time = function (time) {
+    app.time = function (time) {
       return moment(time).format('MMMM Do YYYY')
     }
-    todoList.delete = function (id, index) {
-      console.log(id)
+    app.delete = function (id, index) {
       $http.delete('/api/data/' + id)
         .success(function (data) {
           alert('delete')
-          todoList.getData.splice(index, 1)
+          app.getData.splice(index, 1)
           get()
-        })
-        .error(function (data) {
-          alert('error')
-          console.log('Error: ' + data)
         })
     }
   })
